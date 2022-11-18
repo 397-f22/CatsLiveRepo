be15198c1d9a67c38aca75a204487da508871045
David Zhang <davidzhang2023@u.northwestern.edu>
2022-11-18 16:13:26 -0600

    added formatting to main page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import Apartment from './components/Apartment';
 import data from './temp_data/data.json'
+import Container  from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   return (
     <div className="App">
-      {Object.entries(data).map(([key,unit]) => {
-        return <Apartment data = {unit} key={key}/>
-      })}
-      
+      <h1><span style={{color: 'purple'}}>Cats</span><span style={{color: "gray"}}>Live</span></h1>
+      <Container fluid>
+        <Row>
+          {Object.entries(data).map(([key,unit]) => {
+            return (
+              <Col>
+                <Apartment data = {unit} key={key}/>
+              </Col>
+            )
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }

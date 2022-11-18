dd8bb4d5790c41a8720d17cdfce126276b38bab8
Xiaowei Chen <rinemiya0203@gmail.com>
2022-11-18 17:03:58 -0600

    Add map.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import Apartment from './components/Apartment';
+import Map from './components/Map';
 import data from './temp_data/data.json'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-
+  
   return (
     <div className="App">
       {Object.entries(data).map(([key,unit]) => {
         return <Apartment data = {unit} key={key}/>
       })}
-      
+      <Map />
     </div>
   );
 }
-
 export default App;
